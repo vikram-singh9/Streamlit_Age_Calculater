2e675dd29d8fe4ea75566832dfc7544eadf0ff93
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-04-22 01:29:17 +0500

    Update main.py

--- age-calculater/main.py
+++ age-calculater/main.py
@@ -1,7 +1,13 @@
 import streamlit as st
 from datetime import date
 
-st.title("🎂 Date of Birth Age Calculator")
+st.title("""🎂 **:rainbow[Date of Birth Age Calculator]**""")
+
+st.markdown(f"""
+        **:rainbow[Welcome to my age calculater built with pure python and streamlit.
+        calculate your age by just one click]**
+""")
+
 
 dob = st.date_input("Enter your Date of Birth:", min_value=date(1900, 1, 1), max_value=date.today())
 
@@ -11,7 +17,10 @@ if st.button("calculate age"):
         age = today.year - dob.year
         if (today.month, today.day) < (dob.month, dob.day):
             age -= 1
-        st.success(f"you are {age} years old.")
+        st.markdown(f"""
+        **You are :rainbow[{age} years] old**
+""")
+        st.balloons()
 
 
 
