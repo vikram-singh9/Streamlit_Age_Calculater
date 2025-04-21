ff50f20c2426a48a74586d765c6f36aed31047cf
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-04-21 16:19:37 +0500

    dob app

---
+++ age-calculater/main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from datetime import date
+
+st.title("🎂 Date of Birth Age Calculator")
+
+dob = st.date_input(
+    "Apni Date of Birth select karein:",
+    min_value=date(1900, 1, 1),  # Set earliest allowed date
+    max_value=date.today()       # Prevent future date
+)
+
+if st.button("Calculate Age"):
+    if dob:
+        today = date.today()
+        age = today.year - dob.year
+        if (today.month, today.day) < (dob.month, dob.day):
+            age -= 1
+       
+        st.success(f"Aapki age hai: {age} saal")
